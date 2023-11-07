32f7040aa4ddb3d6b49ab3f04c5c42aa311300be
Alan Carrillo Romero <alan9009@gmail.com>
2023-11-06 23:47:23 -0600

    nestjs-001 : V1.0 entites creator for nestjs

---
+++ server.js
@@ -0,0 +1,60 @@
+const mysql = require('mysql2/promise');
+const fs = require('fs');
+
+const connectionConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'mga',
+};
+
+async function main() {
+  const connection = await mysql.createConnection(connectionConfig);
+
+  try {
+    // Consulta para obtener los nombres de las tablas en la base de datos
+    const [rows] = await connection.query("SELECT table_name FROM information_schema.tables WHERE table_schema = ?", [connectionConfig.database]);
+
+    for (const row of rows) {
+      const tableName = row.TABLE_NAME;
+      console.log(tableName);
+
+      // Consulta para obtener la estructura de la tabla
+      const [tableInfo] = await connection.query(`DESCRIBE ${tableName}`);
+
+      // Transforma la estructura de la tabla en un objeto que será un entity en TypeORM
+      const entityName = tableName;
+      const columns = tableInfo.map((column) => {
+        return {
+          name: column.Field,
+          type: column.Type,
+        };
+      });
+
+      // Genera el código de la entidad con los decoradores @Column
+      const entityCode = `
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class ${entityName}{
+    @PrimaryGeneratedColumn()
+    ${columns.map((column) => `@Column({ type: '${column.type}', name: '${column.name}' })\n    ${column.name}: ${column.type};`).join('\n    ')}
+}
+`;
+
+      // Define el nombre del archivo (puedes personalizar la estructura de nombres si lo deseas)
+      const fileName = `${entityName}.ts`;
+
+      // Escribe el código de la entidad en el archivo
+      fs.writeFileSync(fileName, entityCode);
+
+      console.log(`Archivo ${fileName} creado.`);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    await connection.end();
+  }
+}
+
+main();
